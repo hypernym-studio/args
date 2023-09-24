a6122c370588ea42bb7341692784f50eb07c538b
Ivo Dolenc <ivodolenc@gmail.com>
2023-09-24 21:34:21 +0200

    types: add types

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+// Auto-generated
+export * from '../index.js'
+
+export type Defaults = Record<string, unknown>
+
+export type Args<T = Defaults> = T & {
+  _: string[]
+}
+
+export interface Options {
+  argv?: string[]
+  alias?: Record<string, string | string[]>
+}
