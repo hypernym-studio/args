7c52b94856ea970684d564260da14a5b55686d15
Ivo Dolenc <ivodolenc@gmail.com>
2024-09-10 11:30:05 +0200

    chore: update vitest alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,13 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     include: ['test/**/*.test.ts'],
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
 })
