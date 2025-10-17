44b582448597b363df1ace446dca9ae12de5582c
Ivo Dolenc <ivodolenc@gmail.com>
2025-10-17 14:38:30 +0200

    chore: update dts output path

--- bundler.config.ts
+++ bundler.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '@hypernym/bundler'
 export default defineConfig({
   entries: [
     { input: './src/index.ts' },
-    { dts: './src/types.ts', output: './dist/index.d.mts' },
+    { dts: './src/types.ts', output: './dist/index.d.ts' },
   ],
 })
